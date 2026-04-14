22b98b95418773562bec54747f56fae08d5bd69e
saraabbih1 <abbihsara30@gmail.com>
2026-04-14 16:13:14 +0100

    feat:doctor detail

---
+++ frontend/src/pages/DoctorDetail.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getDoctorById } from "../services/doctorService";
+
+export default function DoctorDetail() {
+  const { id } = useParams();
+  const [doctor, setDoctor] = useState(null);
+
+  useEffect(() => {
+    getDoctorById(id)
+      .then((res) => setDoctor(res.data))
+      .catch((err) => console.log(err));
+  }, [id]);
+
+  if (!doctor) return <p>Loading...</p>;
+
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">{doctor.name}</h1>
+
+      <p> Spécialité: {doctor.specialty}</p>
+      <p> Ville: {doctor.city}</p>
+      <p> Expérience: {doctor.yearsofexperience} ans</p>
+      <p> Prix: {doctor.consultation_price} DH</p>
+      <p> Jours disponibles: {doctor.available_days}</p>
+    </div>
+  );
+}
